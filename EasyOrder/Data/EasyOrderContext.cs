7e6342d3cfd30ad0a020e65709b0d4d4a8db7214
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2026-01-16 14:50:32 -0300

    Criado o Context e BD do sistema

--- EasyOrder/Data/EasyOrderContext.cs
+++ EasyOrder/Data/EasyOrderContext.cs
@@ -9,5 +9,11 @@ namespace EasyOrder.Data
 {
     class EasyOrderContext : DbContext
     {
+        private readonly string _dbContext = @"Data Source=DESKTOP-AF6EDUF\SQLEXPRESSS;Initial Catalog=EasyOrderDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_dbContext);
+        }
     }
 }
